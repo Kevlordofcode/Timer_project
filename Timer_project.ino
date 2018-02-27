b9e66297f5348bdd2a3fe374c83e6c2a1a966e42
Kevlordofcode <kevin@zenweshare.com>
2018-02-27 14:22:59 +0100

    Strucutre avec stepsup et down
    Strucutre avec stepsup et down

--- Timer_project.ino
+++ Timer_project.ino
@@ -1,14 +1,38 @@
 //Timer simple.ino
 #include <Arduino.h>
 
-int temps(4000);
+int ExpTime(4000);
+int Step(2);
+int temps;
+float tempsaffiche;
 float tempsrestant;
 
 #define Start 12
+#define StepUp 11
+#define StepDn 10
 #define Buzz 8
 #define Relais_1_agrand 5
+#define Relais_2_inact 4
+
+	//fonction bip
+void bip(int lb) {
+	digitalWrite(Buzz, HIGH);
+	delay(lb);
+	digitalWrite(Buzz, LOW);
+}
+
+	//Fonction compte à rebours
+void countdown(int temps){
+	unsigned long endtime = millis() + temps;
+	while (millis() < (endtime)) {
+		tempsrestant = (((float)endtime - (float)millis())/1000);
+		Serial.print(tempsrestant, 1);
+		Serial.println();	
+		delay(300);
+	}
 
 
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,7 +40,9 @@ Serial.begin(9600);
 pinMode (Start, INPUT_PULLUP);
 pinMode (Buzz, OUTPUT);
 pinMode (Relais_1_agrand, OUTPUT);
+pinMode (Relais_2_inact, OUTPUT);
 
+	//Son pour dire que c'est initialisé
 Serial.print(temps);
 digitalWrite(Buzz, HIGH);
 delay(500);
@@ -24,27 +50,31 @@ digitalWrite(Buzz, LOW);
 
 }
 
-bip(int lb) {
-	digitalWrite(Buzz, HIGH);
-	delay(lb);
-	digitalWrite(Buzz, LOW);
-	return 0;
-}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  //Step up exposition
+if(digitalRead(StepUp) == LOW) {
+	ExpTime = ExpTime + Step;
+	tempsaffiche = ExpTime/1000;
+	Serial.print(tempsaffiche, 1);
+}
+
+  //Step down exposition
+
+
+  //Déclenche exposition
 if (digitalRead(Start) == LOW) {
-	unsigned long endtime = millis() + temps;
-	while (millis() < (endtime)) {
-	tempsrestant = (((float)endtime - (float)millis())/1000);
-	Serial.print(tempsrestant, 1);
-	Serial.println();	
-	delay(300);
+	digitalWrite(Relais_2_inact, LOW);
+	digitalWrite(Relais_1_agrand, HIGH);
+	countdown(ExpTime);
 	}
 	digitalWrite(Relais_1_agrand, LOW);
-	bip(200);
-	delay(500);
-	}
+	digitalWrite(Relais_2_inact, HIGH);
+	bip(200);								//A supprimer pour version définitive
+	delay(500);								//A supprimer pour version définitive
+	
 
 }
